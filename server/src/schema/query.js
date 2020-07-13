59593aa668a8eee78231c7e41d087c2dff9a0845
David Andika <david@dotdev.com.au>
2020-07-14 00:35:27 +1000

    Added authors query

--- server/src/schema/query.js
+++ server/src/schema/query.js
@@ -1,8 +1,10 @@
 const { getBooks } = require('../model/book');
+const { getAuthors } = require('../model/author');
 
 const typeDef = `
   type Query {
     books(authorId: ID): [Book]
+    authors(bookId: ID): [Author]
   }
 `;
 
@@ -10,6 +12,9 @@ const resolver = {
   Query: {
     books: (parent, args) => {
       return getBooks(args.authorId ? args.authorId.toString() : undefined);
+    },
+    authors: (parent, args) => {
+      return getAuthors(args.bookId ? args.bookId.toString() : undefined);
     },
   },
 };
